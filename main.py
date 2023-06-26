c86048bcc58c386f8df87522d6f70b04cdf921cc
Yun Jang <jang.yunj7@gmail.com>
2023-06-25 23:33:39 -0400

    algorithm2

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ def verify(index):
         print("Target found at index ", index)
     else:
         print("Target not found in the list")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = linear_search(numbers, 12)
+verify(result)
