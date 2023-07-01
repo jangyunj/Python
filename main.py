72e36ca64470576fe679766e01b9fc198bb70a00
Yun Jang <jang.yunj7@gmail.com>
2023-06-30 22:49:42 -0400

    binary search

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-def linear_search(list, target):
-    """
-    Returns the index position of the target if found, else returns None 
-    """
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
 
-    for i in range(0, len(list)):
-        if list[i] == target:
-            return i
-    return None
+    while first <= last:
+        midpoint = (first + last) //2   #floor division operator- rounds down to the nearest whole number
 
-def verify(index):
-    if index is not None:
-        print("Target found at index:", index)
-    else:
-        print("Target not found in list")
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-result = linear_search(numbers, 6)
-verify(result)
-
-result = linear_search(numbers, 12)
-verify(result)
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
