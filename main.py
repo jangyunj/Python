80a4237cd37e1cb5a1c3a33eef21d8dc514509da
Yun Jang <jang.yunj7@gmail.com>
2023-07-06 23:57:19 -0400

    data structure

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-#list needs to be sorted for binary search to work
+#Data structure
 
-def binary_search(list, target):
-    first = 0
-    last = len(list) - 1
+new_list = [1, 2, 3]
+result = new_list[0]
 
-    while first <= last:
-        midpoint = (first + last) //2
+if l in new_list:
+    print(True)
 
-        if list[midpoint] == target:
-            return midpoint
-        elif list[midpoint] < target:
-            first = midpoint + 1
-        else:
-            last = midpoint - 1
-
-    return None
-
-numbers = [1, 2, 3, 4, 5, 6]
-
-def verify(index):
-    if index is not None:
-        print("Target found at:", index)
-    else:
-        print("Target not found in the list")
-
-result = binary_search(numbers, 3)
-verify(result)
+for n in new_list:
+    if n == 1:
+        print(True)
+    break
